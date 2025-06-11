1b0ba3a61df9d4e1f72d44a3d30f9f550e9fb2ff
Cokke <jorgerandomgamer@gmail.com>
2025-06-10 23:40:37 -0400

    fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: procces.env.FB_API_KEY,
-  authDomain: procces.env.FB_AUTH_DOMAIN,
-  projectId: procces.env.FB_PROJECT_ID,
-  storageBucket: procces.env.FB_STORAGE_BUCKET, 
-  messagingSenderId: procces.env.FB_MESSAGING,
-  appId: procces.env.FB_APP_ID,
-  measurementId: procces.env.FB_MEAS_ID,
+  apiKey: process.env.FB_API_KEY,
+  authDomain: process.env.FB_AUTH_DOMAIN,
+  projectId: process.env.FB_PROJECT_ID,
+  storageBucket: process.env.FB_STORAGE_BUCKET, 
+  messagingSenderId: process.env.FB_MESSAGING,
+  appId: process.env.FB_APP_ID,
+  measurementId: process.env.FB_MEAS_ID,
 };
 
 const app = initializeApp(firebaseConfig);
